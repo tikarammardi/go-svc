3af52db583e0ae018d69e8b3b913dca3950bb23c
Tikaram Mardi <tikarammardi007@gmail.com>
2023-10-19 22:05:02 +0530

    maxprocs pkg included to enforce specified quota to be used for pod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tikarammardi/go-svc
 
 go 1.21.1
+
+require go.uber.org/automaxprocs v1.5.3
--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"go.uber.org/automaxprocs/maxprocs"
 	"log"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 )
 
 var build = "develop"
 
 func main() {
-	log.Println("Starting the application...", build)
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(); err != nil {
+		fmt.Println("maxprocs: %w", err)
+		os.Exit(1)
+	}
+
+	g := runtime.GOMAXPROCS(0)
+	log.Printf("Starting the application... build[%s] CPU[%d]\n", build, g)
 	defer log.Println("Ending the application...")
 
 	shutdown := make(chan os.Signal, 1)
